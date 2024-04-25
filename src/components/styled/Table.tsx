1bb1d62f0e4812bf0fb3aa4bdbd828d967d1680e
Carlos Eduardo Sabino de Lima <121118484+KaduLimaz@users.noreply.github.com>
2024-04-25 20:29:27 -0300

    criando table style
    criando table style

---
+++ src/components/styled/Table.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Table = styled.table`
+	width: 100%;
+	border: 1px solid #fff;
+
+	td,
+	th {
+		padding: 10px;
+		border: 1px solid #fff;
+	}
+`;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,30 @@
 import { Fragment } from "react/jsx-runtime";
 import { Title } from "../components/styled/Title";
+import { Table } from "../components/styled/Table";
+
+export interface Produts {
+	id: string;
+	name: string;
+	price: number;
+}
 
 export function Home() {
 	return (
 		<Fragment>
 			<Title>Alô GrowDev</Title>
+			<Table>
+				<thead>
+					<tr>
+						<th>Descrição</th>
+						<th>Preço</th>
+						<th>Quantidade</th>
+					</tr>
+				</thead>
+
+				<tbody>
+					<th></th>
+				</tbody>
+			</Table>
 		</Fragment>
 	);
 }
