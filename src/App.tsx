e78217123a503c0af91ea72209c7893232878907
Carlos Eduardo Sabino de Lima <121118484+KaduLimaz@users.noreply.github.com>
2024-04-25 19:57:35 -0300

    Stylo Global criado

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Fragment } from "react/jsx-runtime";
 import { AppRoutes } from "./config/routes/AppRoutes";
+import { GlobalStyled } from "./global/GlobalStyled";
 
 export function App() {
 	return (
 		<Fragment>
+			<GlobalStyled />
 			<AppRoutes />
 		</Fragment>
 	);
---
+++ src/global/GlobalStyled.tsx
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyled = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    block-size: border-box;
+}
+
+body {
+    background-color: #3a3a3a;
+    width: 100vw;
+    height: 100vh;
+    
+} 
+`;
