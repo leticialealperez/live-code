e36443ad344eedd278f6fd397a8159b227b843a5
Lucas Paulus <lucas.apaulus012@gmail.com>
2024-04-25 20:21:52 -0300

    feat: table created

--- src/components/fuctional/Table.tsx
+++ src/components/fuctional/Table.tsx
@@ -1,12 +1,23 @@
+import { Title } from "../styled/Title"
+
 function Table(){
     return(
-        <table>
-            <tr>
-                <th>Descrição</th>
-                <th>Preço</th>
-                <th>Quantidade</th>                
-            </tr>
-            <th></th>
-        </table>
+        <>
+            <Title>Lista de Produtos</Title>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Descrição</th>
+                        <th>Preço</th>
+                        <th>Quantidade</th>                
+                    </tr>
+                </thead>
+                
+                <tbody>
+                <th></th>
+                </tbody>
+                
+            </table>
+        </>
     )
 }
