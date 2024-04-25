2ac6463a515f83041647c46a05f80c9b17978944
Andrea Noer <andreanoer@gmail.com>
2024-04-25 19:41:20 -0300

    Adiciona lib React-Router-Dom

---
+++ src/config/routes/AppRoutes.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <h1>App</h1>
+    }
+])
+
+export function AppRoutes(){
+    return 
+}
